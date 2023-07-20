656017e2400050083be2394ae8da052a4fb95d32
Celso Lopes <celso.lopes.jr@gmail.com>
2023-07-20 15:32:06 -0300

    Usando a StackNavigator

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # React-Native-Apreendo
+
+============================
+ App-Camera
+Aprendi a utilizar a Camera do Celular Frontal ou Traseira
+Foi preciso pedir permissão e somente será usada se o usuário permiir
+Inclui um captura da foto tirada com botão de fechar.
+Incluir botão de trocar entre a câmera frontal ou traseira
+
+============================
+ App-Caculadora-de-IMC
+App para calcular o IMC com base no Peso e na Altura
+Inclui validações nos inputs
+Inclui registros dos ultimos IMC calculados
+
+============================
+ App-Navegation
+App para aprender a usar os menus de Navegação do Mobile
+Criar páginas e nevagá-las através do Menu
--- my-navegation/App.js
+++ my-navegation/App.js
@@ -1,13 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import React from 'react';
 import styles from './src/global/style';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Contacts from './src/pages/Contacts';
+import Information from './src/pages/Information';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>My Navegation</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Contacts" component={Contacts}/>
+        <Stack.Screen name="Information" component={Information}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
