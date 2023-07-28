4d51a2f15c470d98c81904a7926715b3f57263de
Celso Lopes <celso.lopes.jr@gmail.com>
2023-07-28 01:08:34 -0300

    Finalizando Style e iniciando API com listagem dos últimos preços

--- OnBitCoin/App.js
+++ OnBitCoin/App.js
@@ -1,18 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
 import CurrentPrice from './src/components/CurrentPrice';
 import HistoryGraphic from './src/components/HistoryGraphic';
 import QuotationsList from './src/components/QuotationList';
+import QuotationItems from './src/components/QuotationList/QuotationsItems';
+
+function addZero(number) {
+  if (number <= 9) {
+    return "0" + number;
+  }
+  return number;
+}
+
+function url(qtdDias) {
+  const date = new Date();
+  const listLastDays = qtdDias;
+  const end_date = 
+  `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`;
+  date.setDate(date.getDate() - listLastDays);
+  const start_date = 
+  `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`;
+  //URL  GET API
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&end=${end_date}`;;
+}
+
+
+	
+async function getListCoins(url) {
+  let response = await fetch(url);
+  let retunrApi = await response.json();
+  let selectListQuotations = retunrApi.bpi;
+  const queryCoinsList = Object.keys(selectListQuotations).map((key) => {
+    return {
+      data: key.split("-").reverse().join("/"),
+      valor: selectListQuotations[key],
+    };
+  });
+  let data = queryCoinsList.reverse();
+  return data;
+}
+
+async function getPriceCoinsGraphic(url) {
+  let responseG = await fetch(url);
+  let returnApiG = await responseG.json();
+  let selectListQuotationsG = returnApiG.bpi;
+  const queryCoinsListG = Object.keys(selectListQuotationsG).map((key) => {
+    return selectListQuotationsG[key];
+  });
+  let dataG = queryCoinsListG;
+  return dataG;
+}
 
 export default function App() {
+  const [coinsList, setcoinsList] = useState([]);
+  const [coinsGrafichList, setcoinsGrafichList] = useState([0]);
+  const [days, setdays] = useState(2);
+  const [updateData, setupdateData] = useState(true);
+
+
+  function updateDay(number) {
+    setdays(number);
+    setupdateData(true);
+}
+
+useEffect(() => {
+  getListCoins(url(days)).then((data) => {
+    setcoinsList(data);
+  });
+  getPriceCoinsGraphic(url(days)).then((dataG) => {
+    setcoinsGrafichList(dataG);
+  });
+  if (updateData) {
+    setupdateData(false);
+  }
+}, [updateData]);
+
+
+
+
+
   return (
     <SafeAreaView style={styles.container}>
       
       <StatusBar backgroundColor="#f50d41" barStyle="dark-content" />
       <CurrentPrice/>
       <HistoryGraphic/>
-      <QuotationsList/>
+      <QuotationsList filterDay={updateDay} listTransactions={coinsList}/>
+      
     </SafeAreaView>
   );
 }
---
+++ OnBitCoin/src/components/QuotationList/QuotationsItems/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View, Text, Image, TextComponent}  from 'react-native';
+import styles from './styles';
+
+export default function QuotationItems(props){
+    return(
+        <View style={styles.mainContent}>
+            <View style={styles.contextLeft}>
+                <View style={styles.boxLogo}>
+                    <Image 
+                    style={styles.logBitcoin}
+                    source={require("../../../img/bitcoin.png")}
+                    />
+                    <Text style={styles.dayCotation}>{props.data}</Text>
+                </View>
+            </View>
+            <View style={styles.contextRigth}>
+                <Text style={styles.price}>{props.valor}</Text>
+            </View>
+        </View>
+    )
+}
