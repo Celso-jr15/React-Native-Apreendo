41507f045cff0fc58d19b76ea66ff31edb09a30b
Celso Lopes <celso.lopes.jr@gmail.com>
2023-07-27 22:51:50 -0300

    Estilizando o App com campo para o gráfico e botões do filtro

--- OnBitCoin/App.js
+++ OnBitCoin/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+import CurrentPrice from './src/components/CurrentPrice';
+import HistoryGraphic from './src/components/HistoryGraphic';
+import QuotationsList from './src/components/QuotationList';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <StatusBar backgroundColor="#f50d41" barStyle="dark-content" />
+      <CurrentPrice/>
+      <HistoryGraphic/>
+      <QuotationsList/>
     </SafeAreaView>
   );
 }
